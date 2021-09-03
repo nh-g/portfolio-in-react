3fc829c0e766c493adf812dd92d36931cdf68877
nh-g <giangngohong@gmail.com>
2021-09-03 13:39:11 +0200

    add data

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -31,3 +31,23 @@ export function getHero() {
 }
 
 
+export function getProjectsIntro() {
+  return (
+    <p>
+      Here are the projects I will be making during the Frontend course at
+      Novare Potential.
+    </p>
+  );
+}
+
+export function getSkillsIntro() {
+  return (
+    <p>
+      Here is the techology that I already know and also starting to learn.{" "}
+    </p>
+  );
+}
+
+export function getContactIntro() {
+  return <p>Say Hi for find me on other platforms: </p>;
+}
