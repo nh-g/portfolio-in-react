e7b7b42e47cfaa5b108125d2423ca44abe4eb10b
nh-g <giangngohong@gmail.com>
2021-09-03 23:25:44 +0200

    fix query adjust

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,10 +2,11 @@ import BrandLogo from '../assets/images/NavLogo.png'
 export default function NavigationBar() {
   return (
     <nav>
-      <a href="#hero" className="nav-logo">
-        <img alt="logo" src={BrandLogo} />
-      </a>
-
+      <div className="nav-logo">
+        <a href="#hero">
+          <img alt="logo" src={BrandLogo} />
+        </a>
+      </div>
       <div className="bar">
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
