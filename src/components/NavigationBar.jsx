d0be7e56765a902aebe62afaafb428a90957eb7e
nh-g <giangngohong@gmail.com>
2021-09-14 06:54:35 +0200

    fix nav bar full responsive with theme toggler

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,20 +4,18 @@ import ToggleTheme from './ToggleTheme';
 export default function NavigationBar() {
   return (
     <nav>
-      <div className="nav-logo">
-        <a href="#hero">
+      <div>
+        <a href="#hero" className="nav-logo">
           <img alt="logo" src={BrandLogo} />
         </a>
       </div>
       <div className="bar">
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
-        <div>{/* empty on purpose */}</div>
-
         <a href="#technologies">Tech</a>
         <a href="#contact">Contact</a>
       </div>
-        <ToggleTheme />
+      <ToggleTheme />
     </nav>
   );
 }
