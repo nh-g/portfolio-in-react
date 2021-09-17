628dcbb14a8ed5c538a7feb63e84efb0ec3b6ba7
nh-g <giangngohong@gmail.com>
2021-09-17 15:56:44 +0200

    fix responsive by rem

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -2,8 +2,9 @@
 export function getAboutMe() {
   return (
     <p>
-      I have 5 year experience working in Japan in business development, consulting, project management, for major players in
-      HR and IT. Currently, I am brushing up my skill at Novare Frontend Camp <br />
+      I have 5 year experience working in Japan in business development,
+      consulting, project management, for major players in HR and IT. Currently,
+      I am brushing up my skill at Novare Frontend Camp. <br />
       {/* <br />
       Moving to Stockholm in late of 2018, I took a challenge starting a family
       in a new continent. And with a beautiful chaos since the born of my child and what-called new mom
@@ -11,8 +12,9 @@ export function getAboutMe() {
       <br /> */}
       <br />
       My field of expertise which maybe of use to you, dear visitor, lies in the
-      following: <br />ー UI/UX design <br />ー Responsive and interactive
-      design <br />ー Web development
+      following: <br />
+      <br />❖ UI/UX design <br />❖ Responsive and interactive design
+      <br />❖ Web development
     </p>
   );
 }
