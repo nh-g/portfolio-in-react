aa943ab986475e7a01b8083419eaad7a781bc35f
nh-g <giangngohong@gmail.com>
2021-10-02 22:06:34 +0200

    add hooks to capture escpae from modal by clicking outside

--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -1,15 +1,38 @@
-import { useState } from "react";
+// NPM packages
+import { useState, useEffect, useRef } from "react";
 
+// Project files
 import ImageLoader from "../../components/ImageLoader";
 import Modal from "./Modal";
 import ModalProperties from "./ModalProperties";
 
 export default function ProjectCard({ item }) {
   const { title, isReleased, previewImg } = item;
+
   const [isOpen, setIsOpen] = useState(false);
+
+  /** capture click outside with React Hook */
+  const node = useRef();
+  const handleClick = (e) => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setIsOpen(false);
+  };
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
   return (
     <article>
       <div
+        ref={node}
         onClick={() => isReleased && setIsOpen(true)}
         className={isReleased ? "normal" : "overlay"}
       >
@@ -18,7 +41,7 @@ export default function ProjectCard({ item }) {
             <p>Coming soon</p>
           </div>
         )}
-        
+
         <ImageLoader filePath={previewImg} />
         <h3>{title}</h3>
       </div>
