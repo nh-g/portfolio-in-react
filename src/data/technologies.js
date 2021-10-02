037d82af13e5dde69b64227af69beda9542bbf5b
nh-g <giangngohong@gmail.com>
2021-10-02 22:45:34 +0200

    add more skills

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -1,14 +1,6 @@
 export function getTechnologies() {
   return [
     {
-      imgFilePath: "skills/html5.svg",
-      title: "HTML",
-    },
-    {
-      imgFilePath: "skills/css.svg",
-      title: "Sass",
-    },
-    {
       imgFilePath: "skills/javascript.svg",
       title: "Javascript",
     },
@@ -17,12 +9,24 @@ export function getTechnologies() {
       title: "React",
     },
     {
-      imgFilePath: "skills/figma.svg",
-      title: "Figma",
-    },
-    {
       imgFilePath: "skills/gatsby.svg",
       title: "Gatsby",
+    },
+    {
+      imgFilePath: "skills/typescript.svg",
+      title: "TypeScript",
+    },
+    {
+      imgFilePath: "skills/html5.svg",
+      title: "HTML",
+    },
+    {
+      imgFilePath: "skills/css.svg",
+      title: "Sass",
+    },
+    {
+      imgFilePath: "skills/figma.svg",
+      title: "Figma",
     },
     {
       imgFilePath: "skills/git.svg",
@@ -41,12 +45,20 @@ export function getTechnologies() {
       title: "Design",
     },
     {
-      imgFilePath: "skills/typescript.svg",
-      title: "TypeScript",
-    },
-    {
       imgFilePath: "skills/docker.svg",
       title: "Docker",
+    },
+    {
+      imgFilePath: "skills/firebase.svg",
+      title: "Firebase",
+    },
+    {
+      imgFilePath: "skills/reactTesting.svg",
+      title: "React Testing Library",
+    },
+    {
+      imgFilePath: "skills/jest.svg",
+      title: "Jest",
     },
   ];
 }
