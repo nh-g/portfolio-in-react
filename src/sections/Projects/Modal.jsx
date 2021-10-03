585c39d358a163042e8dd8d8a824f177ec226313
nh-g <giangngohong@gmail.com>
2021-10-03 16:59:40 +0200

    fix bug: close modal on click outside

--- src/sections/Projects/Modal.jsx
+++ src/sections/Projects/Modal.jsx
@@ -4,8 +4,17 @@ export default function Modal({ onClose, open, children }) {
   if (!open) return null;
   return ReactDom.createPortal(
     <>
-      <div className="overlay-modal">
-        <div className="modal">
+      <div
+        className="overlay-modal"
+        onClick={onClose}
+      >
+        <div
+          className="modal"
+          onClick={(e) => {
+            // do not close modal if anything inside modal content is clicked
+            e.stopPropagation();
+          }}
+        >
           <div className="corner-button">
             <button onClick={onClose}>x</button>
           </div>
--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -1,5 +1,5 @@
 // NPM packages
-import { useState, useEffect, useRef } from "react";
+import { useState} from "react";
 
 // Project files
 import ImageLoader from "../../components/ImageLoader";
@@ -11,28 +11,9 @@ export default function ProjectCard({ item }) {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  // /** capture click outside with React Hook */
-  // const node = useRef();
-  // const handleClick = (e) => {
-  //   if (node.current.contains(e.target)) {
-  //     // inside click
-  //     return;
-  //   }
-  //   // outside click
-  //   setIsOpen(false);
-  // };
-  // useEffect(() => {
-  //   document.addEventListener("mousedown", handleClick);
-
-  //   return () => {
-  //     document.removeEventListener("mousedown", handleClick);
-  //   };
-  // }, []);
-
   return (
     <article>
       <div
-        // ref={node}
         onClick={() => isReleased && setIsOpen(true)}
         className={isReleased ? "normal" : "overlay"}
       >
