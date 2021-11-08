9edf0a4f0605de467cfa7033fe51731a527e8b1c
nh-g <giangngohong@gmail.com>
2021-11-08 08:54:53 +0100

    change order of projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,25 +2,6 @@ export function getProjects() {
   return [
     {
       description:
-        "This is a multiple-page website of a vegan restaurant where users can navigate around pages, check address pinned on an interactive map and be able to send a contact form. It uses:",
-      technologies: [
-        "React",
-        "React router dom",
-        "Leaflet map",
-        "Emailjs-com",
-        "React leaflet",
-        "CSS",
-        "React Dom",
-      ],
-      isReleased: true,
-      title: "Elegant Vegan Restaurant",
-      previewImg: "projects/vegan.jpg",
-      appScreenShot: "screenshots/vegan.png",
-      website: "https://veggie-giang.web.app/",
-      github: "https://github.com/nh-g/veggies",
-    },
-    {
-      description:
         "This is a package tracking web application where users can view a list of packages fetched by a mock API; search, sort from the list, view the interactive map tracking all packages' location, switch application's language. It uses:",
       technologies: [
         "fetch API",
@@ -38,20 +19,21 @@ export function getProjects() {
     },
     {
       description:
-        "This is a simple todo list app to allow users to add items to their shopping list. Once an item has been added, you can add a picture for it by pressing on the camera icon; or mark it as complete by pressing on the checkbox. It uses:",
+        "This project is a learning platform where users as admin can do CRUD operation in form of inline editable UI. It uses:",
       technologies: [
-        "Javascript",
-        "React",
-        "ContextAPI",
-        "Local Storage",
-        "Firebase Cloud Storage",
+        "Firebase-authentication",
+        "Cloud Firestore",
+        "Typescript",
+        "SASS",
+        "Context API",
+        "React router dom",
       ],
       isReleased: true,
-      title: "Modern visualizable Todo App",
-      previewImg: "projects/todo.jpg",
-      appScreenShot: "screenshots/todo.png",
-      website: "https://shopping-list-app-eika.web.app/",
-      github: "https://github.com/nh-g/shopping-list",
+      title: "LMS with inline editable Admin dashboard",
+      previewImg: "projects/learning.jpg",
+      appScreenShot: "screenshots/lms.png",
+      website: "https://learning-platform-giang.web.app/",
+      github: "https://github.com/nh-g/lms",
     },
     {
       description:
@@ -93,22 +75,41 @@ export function getProjects() {
     },
     {
       description:
-        "This project is a learning platform where users as admin can do CRUD operation in form of inline editable UI. It uses:",
+        "This is a simple todo list app to allow users to add items to their shopping list. Once an item has been added, you can add a picture for it by pressing on the camera icon; or mark it as complete by pressing on the checkbox. It uses:",
       technologies: [
-        "Firebase-authentication",
-        "Cloud Firestore",
-        "Typescript",
-        "SASS",
-        "Context API",
-        "React router dom",
+        "Javascript",
+        "React",
+        "ContextAPI",
+        "Local Storage",
+        "Firebase Cloud Storage",
       ],
       isReleased: true,
-      title: "LMS with inline editable Admin dashboard",
-      previewImg: "projects/learning.jpg",
-      appScreenShot: "screenshots/lms.png",
-      website: "https://learning-platform-giang.web.app/",
-      github: "https://github.com/nh-g/lms",
+      title: "Modern visualizable Todo App",
+      previewImg: "projects/todo.jpg",
+      appScreenShot: "screenshots/todo.png",
+      website: "https://shopping-list-app-eika.web.app/",
+      github: "https://github.com/nh-g/shopping-list",
     },
+    {
+      description:
+        "This is a multiple-page website of a vegan restaurant where users can navigate around pages, check address pinned on an interactive map and be able to send a contact form. It uses:",
+      technologies: [
+        "React",
+        "React router dom",
+        "Leaflet map",
+        "Emailjs-com",
+        "React leaflet",
+        "CSS",
+        "React Dom",
+      ],
+      isReleased: true,
+      title: "Elegant Vegan Restaurant",
+      previewImg: "projects/vegan.jpg",
+      appScreenShot: "screenshots/vegan.png",
+      website: "https://veggie-giang.web.app/",
+      github: "https://github.com/nh-g/veggies",
+    },
+
     {
       description:
         "This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
